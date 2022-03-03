3d6a21da21e7dc4de1c7ae6621335468a5fe4fd1
João Dutra <joaovitordutrag@gmail.com>
2022-03-03 15:21:18 -0300

    remove service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.min.css';
@@ -13,5 +12,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-serviceWorkerRegistration.unregister();
